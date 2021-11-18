7f1db8fa0882c02dae978ecdbed30f2327e86f78
unknown <luk.jon@tlen.pl>
2021-11-18 14:17:39 +0100

    Created empty directories. Imported new PDF files. Created CSV files from PDF tables.

--- pdftocsv.py
+++ pdftocsv.py
@@ -5,10 +5,11 @@ import os
 # tabula.convert_into("dokument1.pdf", "dokument1.csv", output_format="csv", pages='all')
 # print(df)
 
-for x in os.listdir("pdfs"): 
+for x in os.listdir("pdfs"):
     if (x.endswith(".pdf")):
-        df = tabula.read_pdf("pdfs/" + x, pages='all')[0]
-        tabula.convert_into("pdfs/" + x, "csv/" + x.split(".pdf")[0] + ".csv", output_format="csv", pages='all')
+        df = tabula.read_pdf("./pdfs/" + x, pages='all', encoding="utf-8")
+        print(df)
+        tabula.convert_into("./pdfs/" + x, "csv/" + x.split(".pdf")[0] + ".csv", output_format="csv", pages='all')
         # os.system("mv '" + x + "' pdfs")
         # print(x)
         # print(df)
